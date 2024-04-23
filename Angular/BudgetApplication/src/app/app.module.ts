d2b0a6e46f0e8b2744e5934e9279beac68794c6f
Rourkeyboy <rourkeyboy@gmail.com>
2024-04-23 00:33:13 -0600

    Added routing dependencies
    Added routing dependencies

--- Angular/BudgetApplication/src/app/app.module.ts
+++ Angular/BudgetApplication/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BudgetComponent } from './views/budget/budget.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'budget', component: BudgetComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
